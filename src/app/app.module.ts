ea48dea5077bc87332adb5b14e69aa1c3e44f1d7
madonkey <dermot.s.hughes@gmail.com>
2020-07-23 12:28:50 +0100

    Added font icons, body text, background

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { BodyTextComponent } from './body-text/body-text.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BodyTextComponent
   ],
   imports: [
     BrowserModule
