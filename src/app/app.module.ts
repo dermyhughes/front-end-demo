2599a559ebdf4dae2bba65c4fdddf0a145aba225
madonkey <dermot.s.hughes@gmail.com>
2020-07-24 15:13:22 +0100

    Mostly finished desktop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BodyTextComponent } from './components/body-text/body-text.component';
 import { ContentCardComponent } from './components/content-card/content-card.component';
 import { PageStepperComponent } from './components/page-stepper/page-stepper.component';
 import { PostCardComponent } from './components/post-card/post-card.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { PostCardComponent } from './components/post-card/post-card.component';
     BodyTextComponent,
     ContentCardComponent,
     PageStepperComponent,
-    PostCardComponent
+    PostCardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
